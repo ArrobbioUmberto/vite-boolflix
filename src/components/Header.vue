e42dc236d811f29b2cf584ac3ca67cc0273d6c52
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-07 15:04:30 +0100

    FEAT header components

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script >
+export default {
+
+}
+</script>
+
+<template>
+    <div class="container">
+        <input type="text" placeholder="Cerca il tuo film/serieTv">
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
