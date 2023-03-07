a620179094db6667ec586d67257f694b48156675
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-07 18:51:09 +0100

    FEAT import main

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script >
 // import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue';
+import Main from './components/Main.vue'
 export default {
   components: {
-    Header
+    Header,
+    Main
   }
 }
 </script>
 
 <template>
   <Header></Header>
+  <Main></Main>
 </template>
 
 <style lang="scss" scoped>
