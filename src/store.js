451b06b596959bb059516d3c2e55803eca57a453
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-07 18:50:49 +0100

    FEAT series and vote

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { reactive } from 'vue'
+
+const store = reactive({
+    key: 'cfde1b43701985aba1e8a887fe34189b',
+    search: '',
+    movie: {},
+    series: {},
+    vote: []
+})
+export default store
+
+// const example = 'https://api.themoviedb.org/3/movie/550?api_key=cfde1b43701985aba1e8a887fe34189b'
