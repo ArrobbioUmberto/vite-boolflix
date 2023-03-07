d1ac1e7735fd6d97843488466136d1a4224ca779
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-07 17:55:32 +0100

    FEAT add control with watcher and computed

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -12,27 +12,18 @@ export default {
         };
     },
     methods: {
-        getMovie() {
-            const key = this.store.key;
-            const search = this.store.search;
-            axios.get("https://api.themoviedb.org/3/search/movie", {
-                params: {
-                    api_key: key,
-                    query: search,
-                }
-            })
-                .then((res) => {
-                    console.log(res);
-                    console.log(res.data);
-                    console.log(res.data.results);
-                    this.store.movie = res.data.results;
-                    console.log(this.store.movie)
-                });
+
+        computed: {
+            searchText() {
+                return this.store.search
+            }
+        },
+        watch: {
+            searchText(newvalue, Oldvalue) {
+                console.log('cambio di search', newvalue, Oldvalue)
+            }
         }
-    },
-    created() {
-        this.getMovie();
-    },
+    }
 }
 </script>
 
