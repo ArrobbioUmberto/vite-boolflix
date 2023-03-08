7a624433297a0dcc6c94ab5ca708a8a281bd572b
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-08 15:23:50 +0100

    FEAT standard img size

--- src/store.js
+++ src/store.js
@@ -3,11 +3,9 @@ import { reactive } from 'vue'
 const store = reactive({
     key: 'cfde1b43701985aba1e8a887fe34189b',
     search: '',
-    movie: {},
-    series: {},
+    movie: [],
+    series: [],
     vote: [],
-    image: '',
+    image_size: 'w342',
 })
 export default store
-
-// const example = 'https://api.themoviedb.org/3/movie/550?api_key=cfde1b43701985aba1e8a887fe34189b'
